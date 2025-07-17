6b3e5ebb40bab9b5641b0f86e955ab2c2fe08108
Teeeeeeeerry <czx123czx000@outlook.com>
2025-07-17 19:07:25 +1000

    Update ProjectA.java

--- ProjectA.java
+++ ProjectA.java
@@ -1,12 +1,12 @@
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.List;*/
 
 
 public class ProjectA {
 
     public static void main(String[] args) {
-            Account account = new Account("My Personal Account");
+            //Account account = new Account("My Personal Account");
             boolean running = true;
             while (running) {
             System.out.println("\n===== Personal Finance Manager =====");
@@ -17,28 +17,68 @@ public class ProjectA {
             System.out.println("5. Exit");
             System.out.print("Select option: ");
             
-            switch (In.nextInt()) {
-                case 1:
-                    addIncome(account);
-                    break;
-                case 2:
-                    addExpense(account);
-                    break;
-                case 3:
-                    displayTransactions(account);
-                    break;
-                case 4:
-                    ReportGenerator.generateReport(account);
-                    break;
-                case 5:
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Invalid option");
-            }
         }
     }
 
 }
 
+class Account {
    
+
+
+}   
+
+abstract class Transaction {
+    protected final double amount;
+    protected final String description;
+    protected final String date;
+
+    public Transaction(double amount, String description) {
+        this.amount = amount;
+        this.description = description;
+        this.date = java.time.LocalDate.now().toString();
+    }
+
+    public abstract void displayDetails();
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
+
+class Income extends Transaction {
+    public Income(double amount, String description) {
+        super(amount, description);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.printf("");
+    }
+}
+
+class Expense extends Transaction {
+    private final Category category;
+
+    public Expense(double amount, String description, Category category) {
+        super(amount, description);
+        this.category = category;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.printf("");
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+}
+
+enum Category {
+    FOOD, TRANSPORT, ENTERTAINMENT, OTHER
+}
