100e4df80adb8c57ff47636d35cbd2f8ae71df8e
Teeeeeeeerry <czx123czx000@outlook.com>
2025-07-17 19:00:58 +1000

    Update ProjectA.java

--- ProjectA.java
+++ ProjectA.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 
 public class ProjectA {
-    
+
     public static void main(String[] args) {
-        Account account = new Account("My Personal Account");
-        boolean running = true;
-        
-        while (running) {
+            Account account = new Account("My Personal Account");
+            boolean running = true;
+            while (running) {
             System.out.println("\n===== Personal Finance Manager =====");
             System.out.println("1. Add Income");
             System.out.println("2. Add Expense");
@@ -40,41 +39,6 @@ public class ProjectA {
         }
     }
 
-public class Account {
-    private final String name;
-    private final List<Transaction> transactions = new ArrayList<>();
-
-    public Account(String name) {
-        this.name = name;
-    }
-
-    public void addTransaction(Transaction transaction) {
-        transactions.add(transaction);
-        System.out.println("Transaction added successfully");
-    }
-
-    public void removeTransaction(int index) {
-        if (index >= 0 && index < transactions.size()) {
-            transactions.remove(index);
-            System.out.println("Transaction removed");
-        }
-    }
-
-    public List<Transaction> getTransactions() {
-        return Collections.unmodifiableList(transactions);
-    }
-
-    public List<Transaction> getTransactionsByCategory(Category category) {
-        return transactions.stream()
-                .filter(t -> t instanceof Expense)
-                .filter(e -> ((Expense) e).getCategory() == category)
-                .toList();
-    }
-
-    public void sortTransactions() {
-        transactions.sort(new FinanceComparator());
-    }
 }
-
 
    
