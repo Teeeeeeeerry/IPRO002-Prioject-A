8d9e5941231f87ca89798b0eb4069b5c5570e1d2
Teeeeeeeerry <czx123czx000@outlook.com>
2025-07-20 11:14:53 +1000

    Update ProjectA.java

--- ProjectA.java
+++ ProjectA.java
@@ -33,22 +33,17 @@ class Account {
     private final List<Transaction> transactions = new ArrayList<>();
 
     public Account(String name) {
-
         this.name = name;
-
     }
 
     public void addTransaction(Transaction transaction) {
-
         transactions.add(transaction);
         System.out.println("Transaction added successfully");
-
     }
 
     //delete transaction function should make
     
     public List<Transaction> getTransactions() {
-
         return Collections.unmodifiableList(transactions);
     }
 
@@ -58,9 +53,7 @@ class Account {
     //sort transction
 
     public String getName() {
-
         return name;
-
     }
 
 }
@@ -72,7 +65,6 @@ abstract class Transaction {
     protected final String date;
 
     public Transaction(double amount, String description) {
-
         this.amount = amount;
         this.description = description;
         this.date /* = ? no idea, finish it later*/;
@@ -82,31 +74,23 @@ abstract class Transaction {
     public abstract void displayDetails();
 
     public double getAmount() {
-
         return amount;
-
     }
 
     public String getDate() {
-
         return date;
-
     }
 }
 
 class Income extends Transaction {
 
     public Income(double amount, String description) {
-
         super(amount, description);
-
     }
 
     @Override
     public void displayDetails() {
-
         System.out.printf("");
-
     }
 }
 
@@ -115,28 +99,20 @@ class Expense extends Transaction {
     private final Category category;
 
     public Expense(double amount, String description, Category category) {
-
         super(amount, description);
         this.category = category;
-
     }
 
     @Override
     public void displayDetails() {
-
         System.out.printf("");
-
     }
 
     public Category getCategory() {
-
         return category;
-
     }
 }
 
 enum Category {
-
     FOOD, TRANSPORT, ENTERTAINMENT, OTHER
-
 }
