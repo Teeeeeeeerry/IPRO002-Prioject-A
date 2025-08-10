733ba41ff2c4f5ab47fc2d160b22d43d90f16977
Teeeeeeeerry <czx123czx000@outlook.com>
2025-08-10 15:34:16 +1000

    Update ProjectB.java

--- ProjectB.java
+++ ProjectB.java
@@ -8,7 +8,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
